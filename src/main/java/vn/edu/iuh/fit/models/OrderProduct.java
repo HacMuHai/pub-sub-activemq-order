d1c0b81101d456b7c0c20bdd2832d9288a7cf055
hiep.le <you@example.com>
2024-03-22 15:27:27 +0700

    add models

--- src/main/java/vn/edu/iuh/fit/models/OrderProduct.java
+++ src/main/java/vn/edu/iuh/fit/models/OrderProduct.java
@@ -1,2 +1,24 @@
-package vn.edu.iuh.fit.models;public class OrderProduct {
+package vn.edu.iuh.fit.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "order_product")
+public class OrderProduct implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_product_id")
+    private long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Product product;
+    private int quantity;
+    private String email;
+    private long orderId;
 }
--- src/main/java/vn/edu/iuh/fit/models/Product.java
+++ src/main/java/vn/edu/iuh/fit/models/Product.java
@@ -1,2 +1,32 @@
-package vn.edu.iuh.fit.models;public class Product {
+package vn.edu.iuh.fit.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "proId")
+    private long id;
+    private String name;
+    private int quantity;
+    private double price;
+
+    @ManyToOne
+    private OrderProduct orderProduct;
+
+    public Product(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
 }
