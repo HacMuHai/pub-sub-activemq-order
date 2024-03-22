d70742adbb70787908928908467bcaad804eb208
hiep.le <you@example.com>
2024-03-22 15:26:11 +0700

    Initial commit

---
+++ src/test/java/vn/edu/iuh/fit/models/KtWeek6ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtWeek6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
